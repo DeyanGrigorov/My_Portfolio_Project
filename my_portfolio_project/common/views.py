8b3ae26b4a823a8a34eaebc5e9b946875c9f98fd
Deyan Grigorov <deian1998@abv.bg>
2022-03-07 00:55:32 +0200

    Add index page

--- my_portfolio_project/common/views.py
+++ my_portfolio_project/common/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def landing_page(request):
-    return render(request, 'landing_page.html')
+    return render(request, 'index.html')
